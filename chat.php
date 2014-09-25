d93d6e1db24b424217d32120504e68d395479944
Илья Гусев <mail@igusev.ru>
2014-09-25 23:39:01 +0400

    refresh fix

--- chat.php
+++ chat.php
@@ -3,7 +3,7 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title></title>
-<!--    <meta http-equiv="refresh" content="1">-->
+    <meta http-equiv="refresh" content="2">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <style type="text/css">
         .chat-body {
