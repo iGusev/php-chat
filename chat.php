da7fc1bf56b7c4a9857b043f00d9d3ad85438c76
Илья Гусев <mail@igusev.ru>
2014-09-25 20:51:34 +0400

    первая версия логики

---
+++ chat.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <meta http-equiv="refresh" content="1">
+</head>
+<body>
+<?php echo file_get_contents('text'); ?>
+</body>
+</html>
--- save.php
+++ save.php
@@ -1,1 +1,6 @@
 <?php
+session_start();
+if($_REQUEST['name'] && $_REQUEST['text']) {
+    file_put_contents('text','<div class="sender">'.strip_tags($_REQUEST['name']).'</div><div class="message">'.strip_tags($_REQUEST['text']).'</div>');
+    header('Location: '.$_SERVER['HTTP_REFERER']);
+}
